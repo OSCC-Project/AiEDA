ee632e873e6343dc89eec43c84c7ff77a3fde77b
YihangQiu <qiuyihang131@gmail.com>
2025-08-10 19:20:21 +0800

    fix: feature and vectorization geneation scripts

--- test/test_ieda_vectors.py
+++ test/test_ieda_vectors.py
@@ -19,7 +19,7 @@ from aieda import (
 )
 
 
-lib_paths = [
+LIB_PATHS = [
     "/data/project_share/process_node/dataset_baseline/lib/tcbn28hpcplusbwp30p140ssg0p81v125c_ccs.lib",
     "/data/project_share/process_node/dataset_baseline/lib/tcbn28hpcplusbwp30p140hvtssg0p81v125c_ccs.lib",
     "/data/project_share/process_node/dataset_baseline/lib/tcbn28hpcplusbwp30p140lvtssg0p81v125c_ccs.lib",
@@ -80,27 +80,92 @@ lib_paths = [
     "/data/project_share/process_node/dataset_baseline/lib/ts5n28hpcplvta128x144m2fw_130a_ssg0p81v125c.lib",
     "/data/project_share/process_node/dataset_baseline/lib/tphn28hpcpgv18ssg0p81v1p62v125c.lib",
     "/data/project_share/process_node/dataset_baseline/lib/PLLTS28HPMLAINT_SS_0P81_125C.lib",
-    "/data/project_share/process_node/dataset_baseline/lib/RocketTile_MAX.lib",
-    "/data/project_share/process_node/dataset_baseline/lib/ts1n28hpcplvtb8192x64m8sw_180a_ssg0p81vm40c.lib",
-    "/data/project_share/process_node/T28_lib/mem/ts5n28hpcplvta256x64m2fw_130a_ssg0p9v125c.lib"
+    "/data/project_share/process_node/T28_lib/ccslib/RocketTile_postroute_func_ssg0p81vm40c_cworst_T_setup.lib",
+    "/data/project_share/process_node/T28_lib/ccslib/ts6n28hpcplvta2048x32m8sw_130a_ssg0p81vm40c.lib",
+    "/data/project_share/process_node/T28_lib/ccslib/ts1n28hpcplvtb2048x48m8sw_180a_ssg0p81vm40c.lib",
+    "/data/project_share/process_node/T28_lib/ccslib/ts1n28hpcplvtb8192x64m8sw_180a_ssg0p81vm40c.lib",
+    "/data/project_share/process_node/T28_lib/mem/ts5n28hpcplvta256x64m2fw_130a_ssg0p81v125c.lib",
+    "/data/project_share/process_node/T28_lib/mem/ts1n28hpcplvtb1024x64m4sw_180a_ssg0p81v125c.lib",
+    "/data/project_share/process_node/T28_lib/mem/ts5n28hpcplvta256x128m2fw_130a_ssg0p81v125c.lib",
+    "/data/project_share/process_node/beihai/lib/ts5n28hpcplvta64x88m2fw_130a_ssg0p81v125c.lib",
+    "/data/project_share/process_node/beihai/lib/ts5n28hpcplvta512x64m4sw_130a_ssg0p81v125c.lib",
+    "/data/project_share/process_node/beihai/lib/ts5n28hpcplvta64x84m2fw_130a_ssg0p81v125c.lib"
 ]
 
-def test_vectors_generation(workspace, patch_row_step : int, patch_col_step : int):
+DESIGNS_T28 = [
+        # "/data2/project_share/dataset_baseline/s713/workspace",
+        "/data2/project_share/dataset_baseline/s44/workspace",
+        "/data2/project_share/dataset_baseline/apb4_rng/workspace",
+        "/data2/project_share/dataset_baseline/gcd/workspace",
+        "/data2/project_share/dataset_baseline/s1238/workspace",
+        "/data2/project_share/dataset_baseline/s1488/workspace",
+        "/data2/project_share/dataset_baseline/apb4_archinfo/workspace",
+        "/data2/project_share/dataset_baseline/apb4_ps2/workspace",
+        "/data2/project_share/dataset_baseline/s9234/workspace",
+        "/data2/project_share/dataset_baseline/apb4_timer/workspace",
+        "/data2/project_share/dataset_baseline/s13207/workspace",
+        "/data2/project_share/dataset_baseline/apb4_i2c/workspace",
+        "/data2/project_share/dataset_baseline/s5378/workspace",
+        "/data2/project_share/dataset_baseline/apb4_pwm/workspace",
+        "/data2/project_share/dataset_baseline/apb4_wdg/workspace",
+        "/data2/project_share/dataset_baseline/apb4_clint/workspace",
+        "/data2/project_share/dataset_baseline/ASIC/workspace",
+        "/data2/project_share/dataset_baseline/s15850/workspace",
+        "/data2/project_share/dataset_baseline/apb4_uart/workspace",
+        "/data2/project_share/dataset_baseline/s38417/workspace",
+        "/data2/project_share/dataset_baseline/s35932/workspace",
+        "/data2/project_share/dataset_baseline/s38584/workspace",
+        "/data2/project_share/dataset_baseline/BM64/workspace",
+        "/data2/project_share/dataset_baseline/picorv32/workspace",
+        "/data2/project_share/dataset_baseline/PPU/workspace",
+        "/data2/project_share/dataset_baseline/blabla/workspace",
+        "/data2/project_share/dataset_baseline/aes_core/workspace",
+        "/data2/project_share/dataset_baseline/aes/workspace",
+        "/data2/project_share/dataset_baseline/salsa20/workspace",
+        # "/data2/project_share/dataset_baseline/jpeg_encoder/workspace",
+        # "/data2/project_share/dataset_baseline/eth_top/workspace",
+        # "/data2/project_share/dataset_baseline/yadan_riscv_sopc/workspace",
+        # "/data2/project_share/dataset_baseline/beihai/workspace",
+        # "/data2/project_share/dataset_baseline/shanghai_MS/workspace",
+        # "/data2/project_share/dataset_baseline/nvdla/workspace",
+        # "/data2/project_share/dataset_baseline/ZJU_asic_top/workspace",
+        # "/data2/project_share/dataset_baseline/iEDA_2023/workspace",
+        # "/data2/project_share/dataset_baseline/ysyx4_SoC2/workspace",
+        # "/data2/project_share/dataset_baseline/BEIHAI_2.0/workspace",
+        # "/data2/project_share/dataset_baseline/AIMP/workspace",
+        # "/data2/project_share/dataset_baseline/AIMP_2.0/workspace",
+        # "/data2/project_share/dataset_baseline/ysyx6/workspace",
+        # "/data2/project_share/dataset_baseline/WUKONG/workspace",
+        # "/data2/project_share/dataset_baseline/ysyx4_SoC1/workspace",
+        # "/data2/project_share/dataset_baseline/ysyx4_2/workspace",
+        # "/data2/project_share/dataset_baseline/T1/workspace",
+        # "/data2/project_share/dataset_baseline/T1_machamp/workspace",
+        # "/data2/project_share/dataset_baseline/nanhu-G/workspace",
+        # "/data2/project_share/dataset_baseline/openC910/workspace",
+        # "/data2/project_share/dataset_baseline/T1_sandslash/workspace",
+    ]
+
+
+def test_vectors_generation(workspace, design, patch_row_step : int, patch_col_step : int):
     # step 1 : init by workspace
     data_gen = DataGeneration(workspace)
     
     # step 2 : generate vectors
-    # vectors_dir="/data/project_share/dataset_baseline/eth_top/workspace/output/innovus/vectors"
-    # data_gen.generate_vectors(input_def="/data/project_share/dataset_baseline/eth_top/workspace/output/innovus/result/eth_top_route.def",
-    #                            vectors_dir=vectors_dir)
-    # data_gen.generate_vectors()
+    vectors_dir="/data2/project_share/dataset_baseline/" + design + "/workspace/output/innovus/vectors"
+    input_def = "/data2/project_share/dataset_baseline/" + design + "/workspace/output/innovus/result/" + design + "_route.def"
+    import os
+    if not os.path.exists(input_def):
+        # If uncompressed file not found, try to read compressed DEF file
+        input_def = "/data2/project_share/dataset_baseline/" + design + "/workspace/output/innovus/result/" + design + "_route.def.gz"
     
+    data_gen.generate_vectors(input_def=input_def,
+                               vectors_dir=vectors_dir,
+                               patch_row_step=patch_row_step,
+                               patch_col_step=patch_col_step)
     # data_gen.generate_patterns()
     # vectors_dir="/data/project_share/dataset_baseline/gcd/workspace/output/innovus/vectors"
-    data_gen.generate_vectors(patch_row_step=patch_row_step,
-                              patch_col_step=patch_col_step)
     
-    data_gen.generate_patterns()
+    # data_gen.generate_patterns()
     
 def test_vectors_load(workspace):
     data_load = DataVectors(workspace)
@@ -124,14 +189,26 @@ def test_vectors_load(workspace):
 if __name__ == "__main__":  
     # step 1 : create workspace
     # workspace_dir = "/data/project_share/dataset_baseline/eth_top/workspace"
-    workspace_dir = "/data2/huangzengrong/test_aieda/sky130_1"
+    # workspace_dir = "/data2/project_share/dataset_baseline/s713/workspace"
     # workspace_dir = "/data/project_share/dataset_baseline/gcd/workspace"
-    workspace = workspace_create(directory=workspace_dir, design="gcd")
+    # workspace = workspace_create(directory=workspace_dir, design="s713")
     
-    # workspace.set_libs(lib_paths)
+    # workspace.set_libs(LIB_PATHS)
     
-    test_vectors_generation(workspace, patch_row_step=9, patch_col_step=9)
-    test_vectors_load(workspace)
+    # test_vectors_generation(workspace, patch_row_step=18, patch_col_step=18)
+    # test_vectors_load(workspace)
+    
+    for workspace_dir in DESIGNS_T28:
+        print("process workspace : ", workspace_dir)
+        
+        design = workspace_dir.split("/")[-2]
+        workspace = workspace_create(directory=workspace_dir, design=design)
+        
+        workspace.set_libs(LIB_PATHS)
+        
+        test_vectors_generation(workspace, design, patch_row_step=18, patch_col_step=18)
+        
+        # test_vectors_load(workspace)
 
     exit(0)
 
