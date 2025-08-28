d9302bfc0d78ee85d73abf2f0f2363413e89007f
HongdaZ <987489811@qq.com>
2025-08-28 22:27:55 +0800

    add parameters to parameters.json

--- aieda/data/database/parameters.py
+++ aieda/data/database/parameters.py
@@ -2,43 +2,44 @@
 # -*- encoding: utf-8 -*-
 '''
 @File : parameters.py
-@Author : yell
+@Author : zhanghongda
 @Desc : parameters database
 '''
 import json 
 from dataclasses import dataclass
-
 @dataclass
 class EDAParameters(object):
     """data structure"""
-    def __init__(self):
-        # load default config file
+    def __init__(self, workspace_dir=None):
+        self.workspace_dir = workspace_dir
         self.load_default_params()
     
     def load_default_params(self):
         """load default config file"""
         try:
-            # get current workspace
             import os
-            current_workspace = os.getcwd()
             
-            # build default config file path
-            default_config_file = os.path.join(current_workspace, "config/iEDA_config/pl_default_config.json")
+            if self.workspace_dir is None:
+                self.pl_config = {"PL": {}}
+                return
+
+            default_config_file = os.path.join(self.workspace_dir, "config/iEDA_config/pl_default_config.json")
             
-            with open(default_config_file, 'r') as f:
-                default_config = json.load(f)
-            
-            # load default config
-            if 'PL' in default_config:
-                pl_config = default_config['PL']
-                self.pl_config = {"PL": pl_config}
+            if os.path.exists(default_config_file):
+                with open(default_config_file, 'r') as f:
+                    default_config = json.load(f)
                 
-                
+                if 'PL' in default_config:
+                    pl_config = default_config['PL']
+                    self.pl_config = {"PL": pl_config}
+                else:
+                    self.pl_config = {"PL": {}}
+            else:
+                self.pl_config = {"PL": {}}
                 
         except Exception as e:
-            print(f"warning: failed to load default config file: {e}")
-            self.pl_config = {}
-    
+            self.pl_config = {"PL": {}}
+        
     def sync_to_default_config(self, dse_config):
         try:
             import os
@@ -46,14 +47,13 @@ class EDAParameters(object):
 
             default_config_file = os.path.join(current_workspace, "config/iEDA_config/pl_default_config.json")
             
-
             with open(default_config_file, 'w') as f:
                 json.dump(dse_config, f, indent=4)
             
             print(f"here is the config: {dse_config}")
             
         except Exception as e:
-            print(f"warning: failed to sync to default config file: {e}")
+            pass
     
     
 
