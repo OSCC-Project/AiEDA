5074f7aa9bfea7746e313aa2c6434a4d413458e0
HongdaZ <987489811@qq.com>
2025-08-29 08:59:47 +0800

    update ai/__init__.py: add design_parameter_optimization module imports

--- aieda/ai/__init__.py
+++ aieda/ai/__init__.py
@@ -3,3 +3,4 @@ from .config_base import ConfigBase
 from .net_wirelength_predict.tabnet_config import TabNetDataConfig, TabNetModelConfig
 from .net_wirelength_predict.tabnet_process import TabNetDataProcess
 from .net_wirelength_predict.tabnet_trainer import TabNetTrainer
+from .design_parameter_optimization.dse_facade import DSEFacade
