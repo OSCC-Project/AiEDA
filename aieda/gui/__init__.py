0c93ea1b7c3fc486d7dec1528ea183d70ef16bbf
Yell-walkalone <12112088@qq.com>
2025-09-18 20:33:26 +0800

    fix bug for merge

--- aieda/gui/__init__.py
+++ aieda/gui/__init__.py
@@ -1,19 +1,15 @@
-from .layout import GuiLayout
-<<<<<<< HEAD
-from .workspace import WorkspaceUI
-from .workspaces import WorkspacesUI
-from .net import NetLayout
-from .chip import ChipLayout
-from .layer import LayerLayout
-from .patch import PatchLayout
-from .patches import PatchesLayout
-=======
 try:
+    from .layout import GuiLayout
     from .workspace import WorkspaceUI
+    from .workspaces import WorkspacesUI
+    from .net import NetLayout
+    from .chip import ChipLayout
+    from .layer import LayerLayout
+    from .patch import PatchLayout
+    from .patches import PatchesLayout
 except ImportError:
     # PyQt5 not available, GUI functionality disabled
     WorkspaceUI = None
->>>>>>> e18ee68049f6b33a52c172acc94220daf2944ba4
 
 __all__ = [
     'GuiLayout',
@@ -23,5 +19,5 @@ __all__ = [
     'ChipLayout',
     'LayerLayout',
     'PatchLayout',
-    'PatchLayout'
+    'PatchesLayout'
 ]
